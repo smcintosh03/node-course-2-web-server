e4fa3960c9e7efc19aeba1211fce462dcb6ddc93
Spencer McIntosh <spencermcintosh@Spencers-MacBook-Pro.local>
2018-09-16 13:19:02 -0400

    add pageTitle to projects template

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.get('/about', (req, res) => {
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
     welcomeMessage: 'Welcome to the projects page!'
   });
 });
